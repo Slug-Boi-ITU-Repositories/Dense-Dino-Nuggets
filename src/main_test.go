efceed319e039013130b2bf0ce060bb1aab6eb28
Emilia-Victoria <emiliav.helsted@gmail.com>
2026-02-20 15:59:52 +0100

    Refactor/add_helper_login

--- src/main_test.go
+++ src/main_test.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"net/http"
+	"net/http/cookiejar"
 	"net/url"
 )
 
 const BASE_URL = "http://localhost:5000" // Should probably be changed
 
-func test_register(username, password string, password2 *string, email *string) (*http.Response, error) {
+func helper_register(username, password string, password2 *string, email *string) (*http.Response, error) {
 	//Helper function to register a user
 	if password2 == nil {
 		password2 = &password
@@ -29,4 +30,28 @@ func test_register(username, password string, password2 *string, email *string) 
 		return nil, err
 	}
 	return resp, nil
+}
+
+func helper_login(username string, password string) (*http.Response, *http.Client, error) {
+	//Helper function to login
+	http_session := &http.Client{}
+
+	jar, err := cookiejar.New(nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	http_session.Jar = jar
+
+	r, err := http_session.PostForm(BASE_URL+"/login", url.Values{
+		"username": {username},
+		"password": {password},
+	})
+
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return r, http_session, nil
+
 }
