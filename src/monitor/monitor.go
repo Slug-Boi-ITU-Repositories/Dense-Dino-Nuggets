f9dbec24e6aecc24e7d5c6e6a987354da82a99df
August-Brandt <augustbrandt170@gmail.com>
2026-03-18 16:28:29 +0100

    feat: Change histogram buckets to better fit our usecase

--- src/monitor/monitor.go
+++ src/monitor/monitor.go
@@ -21,6 +21,7 @@ func NewMetrics(reg prometheus.Registerer) *Metrics {
 			prometheus.HistogramOpts{
 				Name: "request_duration",
 				Help: "Duration of processing time for requests to Minitwit in seconds",
+				Buckets: []float64{0.001, 0.01, 0.05, 0.1, 0.2, 0.3, 0.5, 1},
 			}, []string{"path", "method"}),
 	}
 	return m
