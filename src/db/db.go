f714df02b972ffcb6af06127a168d4c9cc341b1d
August-Brandt <augustbrandt170@gmail.com>
2026-03-19 15:40:24 +0100

    fix: Retry database connection in case of issues with connection

--- src/db/db.go
+++ src/db/db.go
@@ -1,7 +1,9 @@
 package db
 
 import (
+	"log"
 	"minitwit/src/model"
+	"time"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -10,16 +12,30 @@ import (
 var DB *gorm.DB
 
 func Connect(dsn string) (*gorm.DB, error) {
-	var err error
-	DB, err = gorm.Open(postgres.Open(dsn))
-	if err != nil {
-		return nil, err
-	}
-	// Auto-migrate models
-	err = DB.AutoMigrate(&model.User{}, &model.Message{}, &model.Follower{})
-	if err != nil {
-		return nil, err
+	log.Println("Connecting to database...")
+	retries := 3
+	for i := range retries {
+		var err error
+		DB, err = gorm.Open(postgres.Open(dsn))
+		if err == nil {
+			log.Println("Database connnection complete")
+			break
+		}
+		if i < 2 {
+			log.Printf("Failed to connect to database [%d/%d]! Retrying in 5 seconds...\n", i+1, retries)
+		} else {
+			return nil, err
+		}
+		time.Sleep(time.Second * 5)
 	}
 
+	// Auto-migrate models
+	log.Println("Running migrations...")
+	err := DB.AutoMigrate(&model.User{}, &model.Message{}, &model.Follower{})
+	if err != nil {
+		log.Printf("Failed to run migrations: %s", err.Error())
+		return nil, err
+	}
+	log.Println("Migrations complete")
 	return DB, nil
 }
