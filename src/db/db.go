ca1d7cd65bfddbc8c623a91536353b40468abff2
Niklas <nizc@itu.dk>
2026-03-19 22:44:25 +0100

    add migrate helper function

--- src/db/db.go
+++ src/db/db.go
@@ -1,12 +1,29 @@
 package db
 
 import (
+	"minitwit/src/model"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
+
+func shouldMigrate(db *gorm.DB) bool {
+    m := db.Migrator()
+
+    if !m.HasTable(&model.User{}) || !m.HasTable(&model.Message{}) || !m.HasTable(&model.Follower{}) {
+        return true
+    }
+
+    if !m.HasColumn(&model.User{}, "pw_hash") ||
+       !m.HasColumn(&model.Message{}, "pub_date") ||
+       !m.HasColumn(&model.Message{}, "flagged") {
+        return true
+    }
+
+    return false
+}
 
 func Connect(dsn string) (*gorm.DB, error) {
 	var err error
@@ -15,11 +32,12 @@ func Connect(dsn string) (*gorm.DB, error) {
 		return nil, err
 	}
 	// Auto-migrate models
-	/*
-	err = DB.AutoMigrate(&model.User{}, &model.Message{}, &model.Follower{})
-	if err != nil {
-		return nil, err
-	}
-	*/
+	if shouldMigrate(DB) {
+    err = DB.AutoMigrate(&model.User{}, &model.Message{}, &model.Follower{})
+    if err != nil {
+        return nil, err
+    }
+}
+
 	return DB, nil
 }
