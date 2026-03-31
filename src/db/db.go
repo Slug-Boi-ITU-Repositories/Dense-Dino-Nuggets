078045b146a2907305c71d79d7a30376bb2e38ba
Philip Han <46556419+Philipnah@users.noreply.github.com>
2026-03-31 11:21:16 +0200

    add uniqueIndex on usernames & add migration code

--- src/db/db.go
+++ src/db/db.go
@@ -12,19 +12,19 @@ import (
 var DB *gorm.DB
 
 func shouldMigrate(db *gorm.DB) bool {
-    m := db.Migrator()
+	m := db.Migrator()
 
-    if !m.HasTable(&model.User{}) || !m.HasTable(&model.Message{}) || !m.HasTable(&model.Follower{}) {
-        return true
-    }
+	if !m.HasTable(&model.User{}) || !m.HasTable(&model.Message{}) || !m.HasTable(&model.Follower{}) {
+		return true
+	}
 
-    if !m.HasColumn(&model.User{}, "pw_hash") ||
-       !m.HasColumn(&model.Message{}, "pub_date") ||
-       !m.HasColumn(&model.Message{}, "flagged") {
-        return true
-    }
+	if !m.HasColumn(&model.User{}, "pw_hash") ||
+		!m.HasColumn(&model.Message{}, "pub_date") ||
+		!m.HasColumn(&model.Message{}, "flagged") {
+		return true
+	}
 
-    return false
+	return false
 }
 
 func Connect(dsn string) (*gorm.DB, error) {
@@ -45,15 +45,24 @@ func Connect(dsn string) (*gorm.DB, error) {
 		time.Sleep(time.Second * 5)
 	}
 
-  log.Println("Running migrations...")
-  if shouldMigrate(DB) {
-    err := DB.AutoMigrate(&model.User{}, &model.Message{}, &model.Follower{})
-    if err != nil {
-        log.Printf("Failed to run migrations: %s", err.Error())
-        return nil, err
-    }
-    log.Println("Migrations complete")
-  }
-  
+	log.Println("Running migrations...")
+	if shouldMigrate(DB) {
+		err := DB.AutoMigrate(&model.User{}, &model.Message{}, &model.Follower{})
+		if err != nil {
+			log.Printf("Failed to run migrations: %s", err.Error())
+			return nil, err
+		}
+	}
+
+	if !DB.Migrator().HasIndex(&model.User{}, "idx_user_username") {
+		err := DB.Migrator().CreateIndex(&model.User{}, "Username")
+		if err != nil {
+			log.Printf("Failed to create username unique index: %s", err.Error())
+			return nil, err
+		}
+	}
+
+	log.Println("Migrations complete")
+
 	return DB, nil
 }
